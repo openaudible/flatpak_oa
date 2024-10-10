9ff9be48ad1d31c92edf80f3d85cf08945b26036
Mazhar Hussain <realmazharhussain@gmail.com>
2024-10-10 18:27:41 +0500

    Working

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ ! -f OpenAudible_x86_64.deb ] && wget https://openaudible.org/latest/OpenAudible_x86_64.deb
+#[ ! -f OpenAudible_x86_64.deb ] && wget https://openaudible.org/latest/OpenAudible_x86_64.deb
 
 # Hard coded version for now.. 
 ./build_flatpak.sh 4.4.7 OpenAudible_x86_64.deb
